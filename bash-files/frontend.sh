58e85e07e8093ee6b8cd9f16f82a11a1e2ca74cc
Giridhar Vedula <giridharvedula@gmail.com>
2023-12-30 15:43:53 +0530

    created the frontend file with commands

--- bash-files/frontend.sh
+++ bash-files/frontend.sh
@@ -1,18 +1,24 @@
-script=$(realpath "$0")
-script_path=$(dirname "$script")
-source ${script_path}/sh-files/common.sh
+#!/bin/bash
 
+# installing nging server 
 yum install nginx -y
 
-cp ${script_path}/roboshop.conf /etc/nginx/default.d/roboshop.conf
+# enble and start ngin server 
+systemctl enable nginx
+systemctl start nginx 
 
+# removing files from nginx host location 
 rm -rf /usr/share/nginx/html/*
 
+# Download the frontend content 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-cd /usr/share/nginx/html
-
+# Change to Nginx directory and extract the frontend content 
+cd /usr/share/nginx/html ||
 unzip /tmp/frontend.zip
 
-systemctl enable nginx
+# Copy Nginx reverse proxy configuration file
+cp cd ../roboshop.conif /etc/nginx/default.d/
+
+# Restart Nginx server to reload the changes
 systemctl restart nginx
