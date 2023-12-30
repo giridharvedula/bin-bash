91d1fc415491b6590cd835b859ba89594d6b27a8
Giridhar Vedula <giridharvedula@gmail.com>
2023-12-30 23:02:53 +0530

    frontend.sh line 18 updated

--- bash-files/frontend.sh
+++ bash-files/frontend.sh
@@ -14,7 +14,8 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 # Change to Nginx directory and extract the frontend content 
-cd /usr/share/nginx/html || 
+# shellcheck disable=SC2164
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 # Copy Nginx reverse proxy configuration file
