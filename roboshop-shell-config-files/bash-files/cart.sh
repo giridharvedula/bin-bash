01fe77a9a2a549742bd533bf310ad3f65dd5f433
Giridhar Vedula <giridharvedula@gmail.com>
2024-01-01 22:53:49 +0530

    created a new folder roboshop-shell-config-files and moved all shell files to that folder

--- roboshop-shell-config-files/bash-files/cart.sh
+++ roboshop-shell-config-files/bash-files/cart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source common.sh
-# Vairable 
+# Variable
 component=cart
 
 # Enable NodeJs 18 module and install 
@@ -13,11 +13,11 @@ useradd roboshop | bash &>>${log_path}
 # Create an app directory, download code from source URL and unzip.
 mkdir /app | bash &>>${log_path}
 curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip | bash &>>${log_path}
-cd /app | bash &>>${log_path}
+cd /app || exit | bash &>>${log_path}
 unzip /tmp/${component}.zip | bash &>>${log_path}
 
-# Install app dipendecy NPM 
-cd /app | bash &>>${log_path}
+# Install app dependency NPM
+cd /app || exit | bash &>>${log_path}
 npm install | bash &>>${log_path}
 
 # Copy the ${component} servie file
@@ -32,3 +32,4 @@ systemctl enable ${component}.service | bash &>>${log_path}
 systemctl start ${component}.service | bash &>>${log_path}
 
 # Udate catalogue server ip address in frontend configuration <roboshop.conf>
+
